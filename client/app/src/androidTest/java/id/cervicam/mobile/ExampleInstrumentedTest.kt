22ee7f60dca54ae10b0d8abc20ef6d799f4def99
Undarkr <97298601+Undarkr@users.noreply.github.com>
2020-12-30 21:13:26 +0000

    Update Makefile.

--- client/app/src/androidTest/java/id/cervicam/mobile/ExampleInstrumentedTest.kt
+++ client/app/src/androidTest/java/id/cervicam/mobile/ExampleInstrumentedTest.kt
@@ -16,4 +16,6 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun useApp
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext

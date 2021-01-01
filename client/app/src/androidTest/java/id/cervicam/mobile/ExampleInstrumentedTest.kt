cd252b186ce4bb73c57921aa1baea271cf58bca5
Undarkr <97298601+Undarkr@users.noreply.github.com>
2021-01-01 13:32:32 +0000

    Update linkpv.lua

--- client/app/src/androidTest/java/id/cervicam/mobile/ExampleInstrumentedTest.kt
+++ client/app/src/androidTest/java/id/cervicam/mobile/ExampleInstrumentedTest.kt
@@ -19,4 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("id.cervicam.mobile"
+        assertEquals("id.cervicam.mobile", appContext.packageName)
+    }
+}

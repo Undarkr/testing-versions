8fbb42f5181aa40586bdc7d9f7126d9f6471b288
Undarkr <97298601+Undarkr@users.noreply.github.com>
2021-01-15 09:53:08 +0000

    add package

---
+++ client/app/src/main/java/id/cervicam/mobile/activities/CameraActivity.kt
@@ -0,0 +1,20 @@
+
+package id.cervicam.mobile.activities
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.MotionEvent
+import android.view.ViewTreeObserver
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat

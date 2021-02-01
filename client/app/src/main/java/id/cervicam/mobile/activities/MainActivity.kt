6aeccf76ec175aba16841452105d4737fca7227c
Undarkr <97298601+Undarkr@users.noreply.github.com>
2021-02-01 15:35:06 +0000

    Update RK Max Flash.lua

--- client/app/src/main/java/id/cervicam/mobile/activities/MainActivity.kt
+++ client/app/src/main/java/id/cervicam/mobile/activities/MainActivity.kt
@@ -2,4 +2,7 @@ package id.cervicam.mobile.activities
 
 import android.content.Intent
 import android.graphics.Color
-import android.os.
+import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+impor

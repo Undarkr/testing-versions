74215e3668fa612bea7556c4ed163474d108c56d
Undarkr <97298601+Undarkr@users.noreply.github.com>
2021-05-15 10:39:37 +0000

    PLFM-3172

--- client/app/src/main/java/id/cervicam/mobile/activities/MainActivity.kt
+++ client/app/src/main/java/id/cervicam/mobile/activities/MainActivity.kt
@@ -51,4 +51,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        s
+        setSupportActionBar(toolbar)
+
+        Utility.setUser(this)
+   

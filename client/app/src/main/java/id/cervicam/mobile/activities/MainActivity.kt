7948420625ae0ff57cb519fb22a282ffcbf7e13b
Undarkr <97298601+Undarkr@users.noreply.github.com>
2021-08-02 07:34:53 +0000

    doc fixed

--- client/app/src/main/java/id/cervicam/mobile/activities/MainActivity.kt
+++ client/app/src/main/java/id/cervicam/mobile/activities/MainActivity.kt
@@ -147,4 +147,6 @@ class MainActivity : AppCompatActivity() {
                                     }
                                 }
                                 waitRequest.countDown()
-                
+                            }
+                        },
+                        query = query

babc26ac321be2ba9a056d38c7a135f208e17dc0
Undarkr <97298601+Undarkr@users.noreply.github.com>
2021-09-11 10:14:51 +0000

    5.6

--- client/app/src/main/java/id/cervicam/mobile/helper/LocalStorage.kt
+++ client/app/src/main/java/id/cervicam/mobile/helper/LocalStorage.kt
@@ -2,4 +2,7 @@ package id.cervicam.mobile.helper
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Share
+import android.content.SharedPreferences
+
+class LocalStorage {
+    enum class PreferenceKeys(val valu

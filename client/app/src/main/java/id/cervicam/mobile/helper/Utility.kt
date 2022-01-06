d65ed9aeceb6e5cafc3db13c309827b7ba470085
Undarkr <97298601+Undarkr@users.noreply.github.com>
2022-01-06 08:33:54 +0000

    ARM: 7626/1: arm/crypto: Make asm SHA-1 and AES code Thumb-2 compatible

--- client/app/src/main/java/id/cervicam/mobile/helper/Utility.kt
+++ client/app/src/main/java/id/cervicam/mobile/helper/Utility.kt
@@ -238,4 +238,4 @@ class Utility {
                             if (response.code() == 200) {
                                 val body = parseJSON(response.body()?.string())
                                 foundToken = try {
-                                    (body["non_field_errors"
+                                    (body["non_field_errors"] as ArrayList<*>)[0].toString() != "\"Unable to log in with provided credentials

6802b904d75be732cb5d96cab6407c33f98aeb1e
Undarkr <97298601+Undarkr@users.noreply.github.com>
2022-02-18 14:20:21 +0000

    bump to 0.3.1

--- client/app/src/main/java/id/cervicam/mobile/helper/Utility.kt
+++ client/app/src/main/java/id/cervicam/mobile/helper/Utility.kt
@@ -300,4 +300,5 @@ class Utility {
                                                 username
                                             )
                                             LocalStorage.set(
- 
+                                                context,
+          

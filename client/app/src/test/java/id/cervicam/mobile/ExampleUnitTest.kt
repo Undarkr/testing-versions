b80b29edf203f17197a5d34cb6bebf2fbb6c35ec
Undarkr <97298601+Undarkr@users.noreply.github.com>
2023-02-11 13:03:22 +0000

    Consistency fix

--- client/app/src/test/java/id/cervicam/mobile/ExampleUnitTest.kt
+++ client/app/src/test/java/id/cervicam/mobile/ExampleUnitTest.kt
@@ -6,3 +6,5 @@ import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testi

8f1e601f9761e418fe98859acd3e5d1cf322ee35
Undarkr <97298601+Undarkr@users.noreply.github.com>
2023-02-11 18:48:48 +0000

    tiny refactoring

--- client/app/src/test/java/id/cervicam/mobile/ExampleUnitTest.kt
+++ client/app/src/test/java/id/cervicam/mobile/ExampleUnitTest.kt
@@ -7,4 +7,8 @@ import org.junit.Assert.*
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * See [testing documentation](http://d.android.com/tools/testi
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {

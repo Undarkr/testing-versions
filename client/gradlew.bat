12ee677453f2aea977ba276142c8a2b404e62c34
Undarkr <97298601+Undarkr@users.noreply.github.com>
2023-04-12 05:27:30 +0000

    Bump version for staging

--- client/gradlew.bat
+++ client/gradlew.bat
@@ -10,4 +10,7 @@ if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
-set APP_
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Add default JVM options here. You can

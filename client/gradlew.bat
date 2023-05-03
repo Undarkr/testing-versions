504be926bf7a0f40e10195f7e42d789ae1736bca
Undarkr <97298601+Undarkr@users.noreply.github.com>
2023-05-03 15:03:11 +0000

    A few bug fixes

--- client/gradlew.bat
+++ client/gradlew.bat
@@ -40,4 +40,7 @@ if exist "%JAVA_EXE%" goto init
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
-echo Please set the JAVA_HOME variable in 
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto

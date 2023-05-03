73a1b5650583bfdd89971075720d322e7c2f6814
Undarkr <97298601+Undarkr@users.noreply.github.com>
2023-05-03 21:59:11 +0000

    Remove unused declaration.

--- client/gradlew.bat
+++ client/gradlew.bat
@@ -43,4 +43,9 @@ echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
 
-goto
+goto fail
+
+:init
+@rem Get command-line arguments, handling Windows variants
+
+if no
